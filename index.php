b696a5ae982d7ffac1ced78aec4a7e2203a6e6da
mengyaxin <yaxin.meng@tophant.com>
2019-01-10 19:48:32 +0800

    phpinfo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include_once ('function.php');
 
 defined('CONF_EXT') or define('CONF_EXT', '.php');
 define('NOW_URL', $_SERVER['REQUEST_URI']);
-define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
+define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']) or define('REQUEST_METHOD','NOT');
 define('HTTP_REFERER', isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '');
 define('IS_GET', REQUEST_METHOD == 'GET' ? true : false);
 define('IS_POST', REQUEST_METHOD == 'POST' ? true : false);
@@ -26,4 +26,6 @@ if (is_file($file)){
     include $file;
 }elseif(!$name){
     echo '没有此文件';
+}else{
+    var_dump($ruleList,$name);
 }
