6dd171f3b1f0f723d1d42faddc21efef64ec100e
孟亚鑫 <yaxin.meng@tophant.com>
2019-09-23 10:57:09 +0800

    21 双列表排序

--- leetcode/21.php
+++ leetcode/21.php
@@ -27,7 +27,6 @@ class ListNode
 
 class Solution
 {
-
     /**
      * @param ListNode $l1
      * @param ListNode $l2
@@ -36,16 +35,35 @@ class Solution
      */
     function mergeTwoLists($l1, $l2)
     {
-        if ($l1->val>$l2->val){
-
+        $dummy = new ListNode( 0 );
+        $ptr   = $dummy;
+        while ($l1 != null || $l2 != null) {
+            if ($l1 == null) {
+                $ptr->next = $l2;
+                break;
+            } elseif ($l2 == null) {
+                $ptr->next = $l1;
+                break;
+            } else {
+                if ($l1->val <= $l2->val) {
+                    $ptr->next = new ListNode( $l1->val );
+                    $l1        = $l1->next;
+                    $ptr       = $ptr->next;
+                } else {
+                    $ptr->next = new ListNode( $l2->val );
+                    $l2        = $l2->next;
+                    $ptr       = $ptr->next;
+                }
+            }
         }
+        return $dummy->next;
     }
 }
 
 
 $a       = new ListNode( 1 );
 $b       = new ListNode( 2 );
-$c       = new ListNode( 3 );
+$c       = new ListNode( 4 );
 $b->next = $c;
 $a->next = $b;
 var_dump( $a );
@@ -56,6 +74,7 @@ $b->next  = $c;
 $aa->next = $b;
 var_dump( $aa );
 $solution = new Solution();
-$solution->mergeTwoLists( $a, $aa );
+$list     = $solution->mergeTwoLists( $a, $aa );
+print_r( $list );
 
 
