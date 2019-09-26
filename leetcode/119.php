a107a9652bc1af63c31ca88bbbc7435765ed633d
孟亚鑫 <yaxin.meng@tophant.com>
2019-09-26 10:28:11 +0800

    119 杨辉三角2 二项式 优化

--- leetcode/119.php
+++ leetcode/119.php
@@ -10,7 +10,7 @@ class Solution
      */
     function getRow($rowIndex)
     {
-        $row=$rowIndex+1;
+        $row = $rowIndex + 1;
         $arr = [];
         for ($i = 0; $i < ceil( $row / 2 ); $i++) {
             $size = $rowIndex;
@@ -27,19 +27,30 @@ class Solution
             }
             $arr[] = $a / $b;
         }
-        if ($row%2){
-            $temp = [array_pop($arr)];
-            $arr = array_merge($arr,$temp,array_reverse($arr));
-        }else{
-            $arr = array_merge($arr,array_reverse   ($arr));
+        if ($row % 2) {
+            $temp = [array_pop( $arr )];
+            $arr  = array_merge( $arr, $temp, array_reverse( $arr ) );
+        } else {
+            $arr = array_merge( $arr, array_reverse( $arr ) );
         }
         return $arr;
+    }
+
+    function one($rowIndex)
+    {
+        $res = [];
+        $cur = 1;
+        for($i=0;$i<=$rowIndex;$i++){
+            $res[] = $cur;
+            $cur = $cur * ($rowIndex-$i) / ($i+1);
+        }
+        return $res;
     }
 }
 
 
 $numRows  = 5;
-$numRows  = 3;
+//$numRows  = 3;
 $solution = new Solution();
-$list     = $solution->getRow( $numRows );
+$list     = $solution->one( $numRows );
 var_dump( $list );
