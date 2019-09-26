5172e4bcf9c4b2e5b4b97a366f4720711d17c6b1
孟亚鑫 <yaxin.meng@tophant.com>
2019-09-26 10:50:48 +0800

    反转链表

--- leetcode/206.php
+++ leetcode/206.php
@@ -8,45 +8,92 @@
 
 //反转链表
 
-include ('../function.php');
 
-class unNodes extends Node{
-    public function unNode($head){
+class unNodes extends Node
+{
+    public function unNode($head)
+    {
         $cur = $head;
-        $i = 0;
+        $i   = 0;
         $arr = [];
-        while(!is_null($cur->next)){
+        while (!is_null( $cur->next )) {
             ++$i;
-            $cur = $cur->next;
+            $cur   = $cur->next;
             $arr[] = $cur->data;
         }
-        $arr = $this->unarr($arr);
-        $head = new  unNodes(null);
-        $head->addNode($head,$arr);
+        $arr  = $this->unarr( $arr );
+        $head = new  unNodes( null );
+        $head->addNode( $head, $arr );
         return $head;
     }
 
     /**
      * 数组反转
+     *
      * @param $arr
+     *
      * @return array|void
      */
     public function unarr($arr)
     {
-        if (!end($arr))
-            return ;
-        $b =[];
-        $b[] = end($arr);
-        while (prev($arr)){
-            $b[] = current($arr);
+        if (!end( $arr ))
+            return;
+        $b   = [];
+        $b[] = end( $arr );
+        while (prev( $arr )) {
+            $b[] = current( $arr );
         }
         return $b;
     }
 }
-$data = new  unNodes(null);
-$a = ['11','22','33','44'];
-$data->addNode($data,$a);
-$data->showNode($data);
-echo '<hr>';
-$node = $data->unNode($data);
-//$data->showNode($node);
+
+
+class ListNode
+{
+    public $val  = 0;
+    public $next = null;
+
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param ListNode $head
+     *
+     * @return ListNode
+     */
+    function reverseList($head)
+    {
+        $p    = null;
+        $curr = $head;
+        while ($curr) {
+            $next       = $curr->next;
+            $curr->next = $p;
+            $p          = $curr;
+            $curr       = $next;
+        }
+        return $p;
+    }
+}
+
+function saveList($arr)
+{
+    $head = new ListNode( '' );
+    $p    = $head;
+    foreach ($arr as $value) {
+        $p->next = new ListNode( $value );
+        $p       = $p->next;
+    }
+    return $head->next;
+}
+
+$arr      = [1, 2, 3, 4, 5];
+$List     = saveList( $arr );
+$Solution = new Solution();
+$Solution->reverseList( $List );
+
