06ccb2054c69255ac4efe2c35dbc557d70c9c169
admin <yaxin.meng@tophant.com>
2020-04-27 11:09:59 +0800

    快速

--- 001/quick_sort.php
+++ 001/quick_sort.php
@@ -10,52 +10,75 @@
 
 function quick_sort($arr)
 {
-    $sarr[0] = ['left'=>0,'right'=>count($arr) - 1];   //该数组保存需要排序的子数组边界
-    $i = 0;
-    $n = 1;
-    while($i < $n){	    //判断还有未排序子数组存在
-        $left = $sarr[$i]['left'];	//数组左边界下标
-        $right = $sarr[$i]['right'];	//数组右边界下标
+    $sarr[ 0 ] = [ 'left' => 0, 'right' => count($arr) - 1 ];   //该数组保存需要排序的子数组边界
+    $i         = 0;
+    $n         = 1;
+    while ($i < $n) {        //判断还有未排序子数组存在
+        $left  = $sarr[ $i ][ 'left' ];    //数组左边界下标
+        $right = $sarr[ $i ][ 'right' ];    //数组右边界下标
         //随机获得关键值
-        $k = rand($left, $right-1);
-        $t = $arr[$left];
-        $arr[$left] = $arr[$k];
-        $arr[$k] = $t;
-        $key = $left;	//关键值
-        $f = false;	        //标志位
-        while($left <= $right) {
-            if($f == false) {
-                if($arr[$right] < $arr[$key]) {
-                    $t = $arr[$key];
-                    $arr[$key] = $arr[$right];
-                    $arr[$right] = $t;
-                    $key = $right;
-                    $f = true;
+        $k            = rand($left, $right - 1);
+        $t            = $arr[ $left ];
+        $arr[ $left ] = $arr[ $k ];
+        $arr[ $k ]    = $t;
+        $key          = $left;    //关键值
+        $f            = false;            //标志位
+        while ($left <= $right) {
+            if ($f == false) {
+                if ($arr[ $right ] < $arr[ $key ]) {
+                    $t             = $arr[ $key ];
+                    $arr[ $key ]   = $arr[ $right ];
+                    $arr[ $right ] = $t;
+                    $key           = $right;
+                    $f             = true;
                 }
                 $right--;
             }
-            if($f) {
-                if($arr[$left] > $arr[$key]) {
-                    $t = $arr[$key];
-                    $arr[$key] = $arr[$left];
-                    $arr[$left] = $t;
-                    $key = $left;
-                    $f = false;
+            if ($f) {
+                if ($arr[ $left ] > $arr[ $key ]) {
+                    $t            = $arr[ $key ];
+                    $arr[ $key ]  = $arr[ $left ];
+                    $arr[ $left ] = $t;
+                    $key          = $left;
+                    $f            = false;
                 }
                 $left++;
             }
         }
         //保存入边界数组
-        if($sarr[$i]['left'] < $key - 1) {
-            $sarr[$n++] = ['left'=>$sarr[$i]['left'], 'right'=>$key-1];
+        if ($sarr[ $i ][ 'left' ] < $key - 1) {
+            $sarr[ $n++ ] = [ 'left' => $sarr[ $i ][ 'left' ], 'right' => $key - 1 ];
         }
-        if($sarr[$i]['right'] > $key + 1) {
-            $sarr[$n++] = ['left'=>$key + 1, 'right'=>$sarr[$i]['right']];
+        if ($sarr[ $i ][ 'right' ] > $key + 1) {
+            $sarr[ $n++ ] = [ 'left' => $key + 1, 'right' => $sarr[ $i ][ 'right' ] ];
         }
         $i++;
     }
     return $arr;
 }
+
+
+function quick($arr)
+{
+    $len = count($arr);
+    if ($len <= 1) {
+        return $arr;
+    }
+
+    $mid   = ceil($len / 2);
+    $left  = [];
+    $right = [];
+    foreach ($arr as $key => $value) {
+        if ($key == $mid) continue;
+        if ($value > $arr[ $mid ]) $right[] = $value;
+        else
+            $left[] = $value;
+    }
+    $left  = quick($left);
+    $right = quick($right);
+    return array_merge($left, [ $arr[ $mid ] ], $right);
+}
+
 
 //示例
 $arr = range(1, 10);
@@ -63,4 +86,5 @@ shuffle($arr);
 print_r($arr);
 
 echo "<br>";
-print_r(quick_sort($arr));
+//print_r(quick_sort($arr));
+print_r(quick($arr));
