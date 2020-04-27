aaab622bca83e616b941ede8649e6b1f5b9df1a3
admin <yaxin.meng@tophant.com>
2020-04-27 11:42:50 +0800

    33.

--- leetcode/33.php
+++ leetcode/33.php
@@ -27,17 +27,17 @@ class Solution
             if ($nums[ $l ] <= $nums[ $mid ]) {
                 if ($nums[ $l ] < $target && $nums[ $mid ] > $target) {
                     $l++;
-                    $r = $mid-1;
+                    $r = $mid - 1;
                 } else {
-                    $l = $mid+1;
+                    $l = $mid + 1;
                     $r--;
                 }
             } else {
                 if ($nums[ $r ] > $target && $nums[ $mid ] < $target) {
-                    $l = $mid+1;
+                    $l = $mid + 1;
                     $r--;
-                }else{
-                    $r = $mid-1;
+                } else {
+                    $r = $mid - 1;
                     $l++;
                 }
             }
@@ -49,7 +49,7 @@ class Solution
 
 
 $arr = [ 4, 5, 6, 7, 0, 1, 2 ];
-$arr = [3,1];
+$arr = [ 3, 1 ];
 //$arr      = [ 8, 7, 6, 5, 4, 3, 2, 1 ];
 //$arr = [4,5,6,7];
 //$arr = [2,4,3,5,1];
